405719a6222b9f2fbc2c02955f9a21ac933b5ef4
Matt Moores <mmoores@gmail.com>
2019-04-12 15:34:57 +1000

    Don't use attach in a vignette

--- R/data.R
+++ R/data.R
@@ -1,3 +1,12 @@
+#' Raman spectrum of methanol (CH3OH)
+#' 
+#' @format A list containing 2 variables:
+#' \describe{
+#'   \item{wavenumbers}{a numeric Vector of 331 wavenumbers (cm^-1)}
+#'   \item{wavenumbers}{a \code{1 * 331} Matrix of intensity values (a.u.)}
+#'  }
+"methanol"
+
 #' SMC particles for TAMRA+DNA (T20)
 #'
 #' Posterior distribution for pseudo-Voigt parameters, obtained by running
@@ -23,3 +32,12 @@
 #' }
 #' @source \url{https://pure.strath.ac.uk/portal/files/43595106/Figure_2.zip}
 "result"
+
+#' SMC particles for methanol (CH3OH)
+#'
+#' Posterior distribution for pseudo-Voigt parameters, obtained by running
+#' `fitVoigtPeaksSMC` on a Raman spectrum of methanol with 4 peaks.
+#' For details, refer to the vignette.
+#'
+#' @format A list containing 15 variables.
+"result2"
